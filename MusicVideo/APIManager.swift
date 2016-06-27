e952c7a92455c08f6a68db5d0e0fa0fb44e78b97
RadX <ralobba@gmail.com>
2016-06-27 23:11:57 +1000

    Completed Part 3

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -20,8 +20,33 @@ class APIManager {
                 if error != nil {
                     completion(result:(error!.localizedDescription))
                 } else {
-                    completion(result:"NSURLSession successful")
-                    print(data)
+                    //Added for JSONSerialization
+                    //print(data)
+                    do {
+                        /* .AllowFragments - top level object is not Array or Dictionary.
+                         Any type of string or value
+                         NSJSONSerialization requires the Do / Try / Catch
+                         Converts the NSDATA into a JSON Object and cast it to a Dictionary */
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                            as? [String: AnyObject] {
+                            
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "JSONSerialization Successful")
+                                }
+                            }
+                        }
+                    } catch {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completion(result: "error in NSJSONSerialization")
+                        }
+                        
+                    }
+                    //End of JSONSerialization
                 }
             }
         }
