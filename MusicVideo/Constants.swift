7e9909ca00e0d4ca2d2eb27b990c54907f11df64
RadX <ralobba@gmail.com>
2016-06-27 23:56:33 +1000

    Part 4 Completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Rex Jason Alobba on 27/6/16.
+//  Copyright © 2016 Rex Jason Alobba. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
