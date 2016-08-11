fde85a1d4fdbc9b9c6572b98af16dd06da9364de
RadX <ralobba@gmail.com>
2016-08-11 22:58:16 +1000

    Completed Part 6

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -9,16 +9,15 @@
 import Foundation
 
 class APIManager {
-    func loadData(urlString: String, completion: (result:String) -> Void) {
+    func loadData(urlString: String, completion: [Videos] -> Void) {
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
         let session = NSURLSession(configuration: config)
 //        let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue()){
                 if error != nil {
-                    completion(result:(error!.localizedDescription))
+                    print(error!.localizedDescription)
                 } else {
                     //Added for JSONSerialization
                     //print(data)
@@ -28,27 +27,33 @@ class APIManager {
                          NSJSONSerialization requires the Do / Try / Catch
                          Converts the NSDATA into a JSON Object and cast it to a Dictionary */
                         
-                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-                            as? [String: AnyObject] {
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary,
+                            feed = json["feed"] as? JSONDictionary,
+                            entries = feed["entry"] as? JSONArray {
                             
-                            print(json)
+                            var videos = [Videos]()
+                            for entry in entries {
+                                let entry = Videos(data: entry as! JSONDictionary)
+                                videos.append(entry)
+                            }
+                            
+                            let i = videos.count
+                            print("iTunesApiManager - total count --> \(i)")
+                            print(" ")
                             
                             let priority = DISPATCH_QUEUE_PRIORITY_HIGH
                             dispatch_async(dispatch_get_global_queue(priority, 0)) {
                                 dispatch_async(dispatch_get_main_queue()) {
-                                    completion(result: "JSONSerialization Successful")
+                                    completion(videos)
                                 }
                             }
                         }
                     } catch {
-                        dispatch_async(dispatch_get_main_queue()) {
-                            completion(result: "error in NSJSONSerialization")
-                        }
-                        
+                        print("error in NSJSONSerialization")                        
                     }
                     //End of JSONSerialization
                 }
-            }
+            
         }
         task.resume()
     }
--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -10,25 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var videos = [Videos]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Call API
         let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/genre=1622/json") {
-            (result: String) in
-            let alert = UIAlertController(title: (result), message: nil, preferredStyle: .Alert)
-            let okAction = UIAlertAction(title: "Ok", style: .Default) {
-                action -> Void in
-            }
-            alert.addAction(okAction)
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/genre=1622/json",
+            completion: didLoadData)
 
     }
 
-//    func didLoadData(result: String) {
-//        print(result)
-//    }
+    func didLoadData(videos: [Videos]) {
+        self.videos = videos
+        for item in videos {
+            print("name = \(item.vName)")
+        }
+        myTest()
+    }
+    
+    func myTest() {
+        for item in videos {
+            print("Mytest - name = \(item.vName)")
+        }
+
+        for (index, item) in videos.enumerate() {
+            print("\(index) name = \(item.vName)")
+        }
+//        for i in 0..<videos.count {
+//            let video = videos[i]
+//            print("\(i) name = \(video.vName)")
+//        }
+    }
 
 
 }
