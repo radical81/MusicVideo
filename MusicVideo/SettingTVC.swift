5116389add7619abe8141a30a2628941a5163ffd
RadX <ralobba@gmail.com>
2017-02-15 22:41:39 +1100

    Part 24 complete

--- MusicVideo/SettingTVC.swift
+++ MusicVideo/SettingTVC.swift
@@ -24,6 +24,18 @@ class SettingTVC: UITableViewController {
         title = "settings"
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "preferredFontChanged", name: UIContentSizeCategoryDidChangeNotification, object: nil)
         touchId.on = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("APICNT") != nil) {
+            let theValue = NSUserDefaults.standardUserDefaults().objectForKey("APICNT") as! Int
+            APICnt.text = "\(theValue)"
+            sliderCnt.value = Float(theValue)
+        }
+    }
+    
+    @IBAction func valueChanged(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(Int(sliderCnt.value), forKey: "APICNT")
+        APICnt.text = ("\(Int(sliderCnt.value))")
     }
     
     @IBAction func touchIdSecurity(sender: UISwitch) {
